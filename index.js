4d704ec1d2556db215df747d3380c6b15d417bd4
giacomotolari <tolarigiacomo@gmail.com>
2022-11-09 09:53:11 +0100

    update

--- index.js
+++ index.js
@@ -4,36 +4,14 @@ const list = document.querySelector("ul");
 const URL_API =
   "https://raw.githubusercontent.com/giacomotolari/json-fetch/main/data.json";
 
-// button.addEventListener("click", () => {
-//   fetch(URL_API)
-//     .then((response) => response.json())
-//     .then((data) =>
-//       data.map((person) => {
-//         return (list.innerHTML += `<li>${person.firstName} - ${person.lastName} -${person.age}</li>`);
-//       })
-//     );
-// });
-
-// button.addEventListener("click", async () => {
-//   try {
-//     const response = await fetch(URL_API);
-//     const data = await response.json();
-//     list.innerHTML.length === 0 && data.map((person) => {
-//       return (list.innerHTML += `<li>${person.firstName} - ${person.lastName} -${person.age}</li>`);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-
 button.addEventListener("click", async () => {
   try {
     const response = await fetch(URL_API);
     const data = await response.json();
-    list.innerHTML.length === 0 && data.map((person) => {
-      return (list.innerHTML += `<li>${person.firstName} - ${person.lastName} -${person.age}</li>`);
-    });
+    list.innerHTML.length === 0 &&
+      data.map((person) => {
+        return (list.innerHTML += `<li>${person.firstName} - ${person.lastName} -${person.age}</li>`);
+      });
   } catch (err) {
     console.log(err);
   }
